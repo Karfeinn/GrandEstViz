c3147481d02b389a6ca7b15fd22c818945c96076
Karfeinn <kayn.akabane@gmail.com>
2026-02-11 11:55:46 +0100

    Création des onglets

--- app.py
+++ app.py
@@ -8,7 +8,21 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MINTY])
+
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(html.H1("La vie avifaune du Grand Est", className="text-primary fw-bold text-center"), width=12)
+    ]),
+
+    dbc.Tabs([
+        # Onglet Pouvoirs Publics
+        dbc.Tab(label="Pouvoirs Publics", tab_id="tab-pp", children=[]),
+        
+        # Onglet Grand Public
+        dbc.Tab(label="Grand Public", tab_id="tab-gp", children=[])
+    ])
+])
 
 if __name__ == '__main__':
     app.run(debug=True)
